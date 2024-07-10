9fb2eef1121f8134173e686bedec7bb5a335a6d8
Catalina Krenz <caty_kr_enz@hotmail.com>
2024-07-10 15:32:27 -0300

    proyecto final

---
+++ client frontend/src/components/homeComponents/CalltoActionSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CalltoActionSection = () => {
+  return (
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="subscribe-head">
+              <h2>¿Necesitas más consejos?</h2>
+              <p>Regístrate gratis y obtén los últimos consejos.</p>
+              <form className="form-section">
+                <input placeholder="Tu email..." name="email" type="email" />
+                <input value="Sí, quiero!" name="subscribe" type="submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalltoActionSection;
